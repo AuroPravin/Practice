ed993ad069cc4fd12737eab13aaa3cc61801ec43
PRAVIN R <61355675+AuroPravin@users.noreply.github.com>
2022-11-01 20:20:18 +0530

    Add files via upload

---
+++ practice c/ADD.C
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+ int x=0,y=0,z=0;
+ clrscr();
+ printf("Enter the value of x :");
+ scanf("%d",&x);
+ printf("Enter the value of y :");
+ scanf("%d",&y);
+ z=x+y;
+ printf("The sum of %d & %d is %d",x,y,z);
+ getch();
+}
